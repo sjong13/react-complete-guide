210764871a5856b4a57e9f7ecb085c07f07da9e7
Stephen Jong <stephen.jong@improving.com>
2022-03-29 14:56:21 -0500

    end of section 3

---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+﻿import './Card.css';
+
+const Card = (props) => {
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
